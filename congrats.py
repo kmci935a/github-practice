0491d7538c1afbc7aaadca016e90c9aed5ead8f9
Jenny Farver <jfarver@civisanalytics.com>
2017-03-09 20:35:34 -0600

    Update congrats.py

--- congrats.py
+++ congrats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-WINTER_2017 = ['K. Student', 'J. Student', 'L. Student', 'M. Student', 'N. Student']
+WINTER_2017 = ['K. Student', 'J. Student', 'L. Student', 'M. Student', 'Z. Student']
 # ADD A NEW LINE HERE TO ADD YOURSELF TO THE LIST using +=
 
 def wish_congrats(class_list) :
