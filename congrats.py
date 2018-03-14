15df3e538fdc59a3f814e78564600a78008043da
Jenny Farver <jfarver@popularpays.com>
2018-03-13 20:45:16 -0500

    Update congrats.py

--- congrats.py
+++ congrats.py
@@ -3,11 +3,11 @@
 WINTER_2018 = ['K. Student', 'J. Student', 'L. Student', 'M. Student', 'Z. Student']
 # ADD A NEW LINE HERE TO ADD YOURSELF TO THE LIST using +=
 
-print 'YAY!'
+print('YAY!')
 
 def wish_congrats(class_list) :
 	for students in class_list: 
-		print 'Congrats %s!  You completed Programming for Data Analytics!' % students
+		print 'Congrats {}!  You completed Programming for Data Analytics!'.format(students)
 
-print 'YOU GUYS ARE AWESOME!!!'
-wish_congrats(WINTER_2017)
+print('YOU GUYS ARE AWESOME!!!')
+wish_congrats(WINTER_2018)
